e027e0cc9b154c58bc535ad9d59cd0ef36e4ff76
SripadDV <sripadv96@gmail.com>
2026-09-18 22:57:01 +0530

    Solved Detect Loop in LinkedList

---
+++ LinkedList/Detect_Loop_In_LinkedList.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head == None:
+            return False
+        
+        fast = head
+        slow = head
+        while fast.next != None:
+            fast = fast.next
+            slow = slow.next
+            if fast.next == None:
+                return False
+            fast = fast.next
+            if fast == slow:
+                return True
+        return False
